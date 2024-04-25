4a93941b9a5a5a56989936b3db72621c06652ec7
Daniel Khashabi <danielk@jhu.edu>
2024-04-24 21:25:01 -0400

    minor.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+import transformers
 import requests
+import torch
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
@@ -7,7 +9,9 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
 url = "http://ia1.wse.jhu.edu:8080/v1/completions"
+model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
 headers = {"Content-Type": "application/json"}
+
 
 @app.event("app_mention")
 def event_test(say, body):
@@ -19,20 +23,22 @@ def event_test(say, body):
         user_id = body["authorizations"][0]["user_id"]
         body_without_user_id = body["event"]["text"].replace(f"<@{user_id}>", "")
         print("The message is: %s" % body_without_user_id)
+        thread_ts = body["event"]["thread_ts"]
 
         data = {
-            "model": "meta-llama/Meta-Llama-3-8B-Instruct",
+            "model": model_name,
             "prompt": body_without_user_id,
             "max_tokens": 200,
             "temperature": 0,
-            "stop_token_ids": [128001, 128009]
+            "stop_token_ids": [128001, 128009],
+            "eos_token_id": [128001, 128009],
         }
-
 
         response = requests.post(url, headers=headers, json=data)
         print(response.json())
         output = response.json()["choices"][0]["text"]
-        say(output)
+        # say this output in the thread
+        say(text=output, thread_ts=thread_ts)
     except Exception as e:
         print("Error: %s" % e)
         say("I'm sorry, I don't know the answer to that question. Here is an error message: ```%s```" % e)
